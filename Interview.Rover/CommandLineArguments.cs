83fb01105bbe017cdf2e8e6afc121811e7a168f3
Alexander Johnston <ajohnston@ulabs.io>
2018-06-25 19:18:04 -0400

    Program flow, command line argument parsing, and final cleanup.

---
+++ Interview.Rover/CommandLineArguments.cs
@@ -0,0 +1,83 @@
+﻿#region header
+
+// Interview.Rover - CommandLineArguments.cs
+// 
+// Authored by Alexander Johnston for practice and not intended to be used by anyone except for evaluation.
+// 
+// Created: 2018-06-25 4:40 PM
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Interview.Rover.Grid;
+using Interview.Rover.Logic;
+
+namespace Interview.Rover
+{
+    public class CommandLineArguments
+    {
+        /// <summary>
+        ///     Determines the size of the grid and is populated by <see cref="ParseGridSize" />.
+        ///     Formatted as "X Y".
+        ///     This parameter isn't actually used because I don't have enough information to decide if it's necessary.
+        ///     There is not an acceptable output listed in the challenge for "fell off the grid", so I'm going to process instructions
+        ///     as if the grid is infinite or theoretical.
+        /// </summary>
+        public int[] GridSize { get; set; } = {0, 0};
+
+        /// <summary>
+        ///     A list of starting positions for rovers in the form "X Y Z" with Z being a <see cref="Compass" />
+        /// </summary>
+        public List<string> StartingPositions { get; set; } = new List<string>();
+
+        /// <summary>
+        ///     A list of movement instructions for rovvers of any length, containing commands 'L', 'M', and 'R'.
+        /// </summary>
+        public List<string> MovementInstructions { get; set; } = new List<string>();
+
+        /// <summary>
+        ///     Parses out both starting positions and movement instructions.
+        ///     Items must be weaved so that each starting position is followed by movement instructions.
+        ///     For example: 1 2 1 2 1 2 1 2, with 1 being starting position and 2 being movement instruction.
+        /// </summary>
+        /// <param name="args">Ordered sets of positions and movement instructions.</param>
+        public void ParseInstructions(string pathToFile)
+        {
+            try
+            {
+                using (StreamReader sReader = new StreamReader(pathToFile))
+                {
+                    string gridSize = sReader.ReadLine(); // Unused currently.
+                    while (sReader.Peek() >= 0)
+                    {
+                        StartingPositions.Add(sReader.ReadLine());
+                        MovementInstructions.Add(sReader.ReadLine());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Could not read the input file {pathToFile}.");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        ///     Parses grid instructions from "MaxX MaxY" format "X Y" to their respective values in <see cref="GridSize"/>.
+        /// </summary>
+        /// <param name="size">A string formatted as "X Y" with the integers representing maximum grid length along that axis.</param>
+        public void ParseGridSize(string size)
+        {
+            var x = size.Substring(0, 1);
+            var y = size.Substring(2, 1);
+            if (int.TryParse(x, out var X))
+                GridSize[0] = X;
+            if (int.TryParse(y, out int Y))
+                GridSize[1] = Y;
+        }
+    }
+}
--- Interview.Rover/Program.cs
+++ Interview.Rover/Program.cs
@@ -10,6 +10,7 @@
 
 using System;
 using System.IO;
+using System.Linq;
 using Interview.Rover.Vehicle;
 
 namespace Interview.Rover
@@ -18,25 +19,96 @@ namespace Interview.Rover
     {
         private static void Main(string[] args)
         {
-
-        }
-
-        private static void ExecuteAndLogToFile(LandRover rover, string movementInstructions, string filename)
-        {
-            var file = new FileStream($"{AppDomain.CurrentDomain.BaseDirectory}{filename}.txt", FileMode.OpenOrCreate);
-            var consoleOutput = Console.Out;
-            using (var sWriter = new StreamWriter(file))
+            var cmdArgs = new CommandLineArguments();
+            if (args[0].ToLower().Contains("help") || args[0].ToLower().Contains("man"))
             {
-                Console.SetOut(sWriter);
+                ShowHelp();
+                return;
+            }
+            else
+                cmdArgs.ParseInstructions(args[0]);
 
+            string outputFileName = $"{AppDomain.CurrentDomain.BaseDirectory}";
+            if (args[1].Length > 0)
+                outputFileName = args[1];
+            else
+                outputFileName += "output.txt";
+
+            //  Iterates over all current rovers imported from file-based command arguments.
+            for (int currentRover = 0; currentRover < cmdArgs.StartingPositions.Count; currentRover++)
+            {
+                var rover = InitializeRoverAtPosition(cmdArgs.StartingPositions.ElementAt(currentRover));
+                ExecuteAndLogToFile(rover, cmdArgs.MovementInstructions.ElementAt(currentRover), outputFileName);
             }
         }
 
-        private static LandRover InitializeRover(string startingPosition)
+        /// <summary>
+        ///     Outputs the <see cref="Console" /> to a file located in the <see cref="AppDomain" /> base directory.
+        /// </summary>
+        /// <param name="rover">A <see cref="LandRover" /> to be moved.</param>
+        /// <param name="movementInstructions">A list of instructions to be passed to the <see cref="LandRover" />.</param>
+        /// <param name="fileName">
+        ///     The name of the file containing output strings which report the <see cref="LandRover" /> current
+        ///     position after each instruction.
+        /// </param>
+        private static void ExecuteAndLogToFile(LandRover rover, string movementInstructions, string fileName)
         {
-            LandRover rover = new LandRover();
-            return rover;
+            FileStream file;
+            if (File.Exists(fileName))
+                file = new FileStream($"{fileName}", FileMode.Append);
+            else
+                file = new FileStream($"{fileName}", FileMode.OpenOrCreate);
+            var consoleOutput = Console.Out;
+            using (var sWriter = new StreamWriter(file))
+            {
+                //  Trace the console out to the file.
+                Console.SetOut(sWriter);
+                var currentPos = rover.ExecuteInstructions(movementInstructions);
+                Console.WriteLine(currentPos);
+
+                //  Restore the original console output.
+                Console.SetOut(consoleOutput);
+            }
         }
 
+        private static void ShowHelp()
+        {
+            Console.WriteLine(@"Welcome to the Mars Rover interview challenge.
+This program was built by Alexander Johnston and submitted 2018-25-06.
+
+HOW TO USE:
+---
+The first argument should contain the fully qualified path to your input instructions as a plaintext file.
+The second argument specifies an output file, otherwise the program will output to the AppDomain's base directory.
+
+EX: C:\Working\RoverInstructions.txt C:\Working\Output.txt
+
+INSTRUCTIONS FORMAT:
+---
+MaxX MaxY           <- Only include this once at the top of the file to determine boundaries.
+PosX PosY FaceZ     <- You can include one starting position per Rover, but this line can be repeated after the next one for multiple rovers.
+LMRLMRLMRLMRLMRLMR  <- You can include only one movement set for each rover, but you can include as many rovers as you want.
+
+MaxX = maximum X coordinate
+MaxY = maximum Y coordinate
+PosX = starting position X
+PosY = starting position Y
+FaceZ = facing direction ['N, E, S, W'] <- Choose one
+
+EX: 5 1 N
+    LMLMLMLMM
+
+OUTPUT:
+---
+CurrentPosX CurrentPosY CurrentFaceZ
+
+EX: 1 3 E
+
+");
+        }
+
+        private static LandRover InitializeRoverDefault() => new LandRover();
+
+        private static LandRover InitializeRoverAtPosition(string startingPosition) => new LandRover(startingPosition);
     }
 }
